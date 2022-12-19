de235a07c221a9a21ed027233028228bc22947a8
Lucas Knupp <lucas_knupp@outlook.com>
2022-12-19 19:22:17 -0300

    Terceiro commit, configurando o banco de dados

--- index.js
+++ index.js
@@ -33,3 +33,12 @@ const storage = multer.diskStorage({
   }
 })
 const upload = multer({ storage });
+
+/* MONGOOSE */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect.`));
