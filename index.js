def2c0ad9759b71cb35893e85d1dd3887375308e
Lucas Knupp <lucas_knupp@outlook.com>
2022-12-19 20:13:10 -0300

    Autenticação e Verificação de usuários

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from "url";
+import authRotes from "./routes/auth.js"
+import { register } from './controllers/auth.js'
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +36,12 @@ const storage = multer.diskStorage({
   }
 })
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRotes);
 
 /* MONGOOSE */
 const PORT = process.env.PORT || 6001;
