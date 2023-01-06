ad36f09faffd67592c40fcfa21343e6b9ad9f6e5
Lucas Knupp <lucas_knupp@outlook.com>
2023-01-06 14:49:51 -0300

    Configuração das propriedades css do DarK/Light Mode

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,27 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import ProfilePage from 'scenes/profilePage';
 import LoginPage from 'scenes/loginPage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from 'theme';
+
 
 function App() {
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
     return <div className='app'>
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<LoginPage />} />
-                <Route path='/home' element={<HomePage />} />
-                <Route path='/profile/:userId' element={<ProfilePage />} />
-            </Routes>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Routes>
+                    <Route path='/' element={<LoginPage />} />
+                    <Route path='/home' element={<HomePage />} />
+                    <Route path='/profile/:userId' element={<ProfilePage />} />
+                </Routes>
+            </ThemeProvider>
         </BrowserRouter>
     </div>;
 }
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
+
 const Navbar = () => {
-    return (<div>Navbar</div>)
+    const [isMobileMenuToggled, setIsMoblieMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+    return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+        <FlexBetween gap="1.75rem"></FlexBetween>
+    </FlexBetween>
 }
 
 export default Navbar;
