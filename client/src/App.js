96bcbdb39da6639612e3d98c97da8fac69020a0b
Lucas Knupp <lucas_knupp@outlook.com>
2023-01-06 13:29:11 -0300

    configurações do Redux e do Redux/toolkit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import ProfilePage from 'scenes/profilePage';
+import LoginPage from 'scenes/loginPage';
 
 function App() {
-    return <div className='app'></div>;
+    return <div className='app'>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<LoginPage />} />
+                <Route path='/home' element={<HomePage />} />
+                <Route path='/profile/:userId' element={<ProfilePage />} />
+            </Routes>
+        </BrowserRouter>
+    </div>;
 }
 
 export default App;
