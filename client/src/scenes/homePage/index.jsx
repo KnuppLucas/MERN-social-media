9b790a6727cb15e2e09f076fd33c5214d2945638
Lucas Knupp <lucas_knupp@outlook.com>
2023-01-09 13:25:15 -0300

    Widget do usuário

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,44 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    const { _id, picturePath } = useSelector((state) => state.user);
+
     return (
         <Box>
             <Navbar />
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreens ? "flex" : "block"}
+                gap="0.5rem"
+                justifyContent="space-between"
+            >
+                <Box
+                    flexBasis={isNonMobileScreens ? "26px" : undefined}
+                >
+                    <UserWidget
+                        userId={_id}
+                        picturePath={picturePath} 
+                    />
+                </Box>
+                <Box 
+                    flexBasis={isNonMobileScreens ? "42px" : undefined}
+                    mt={isNonMobileScreens ? undefined : "2rem"}
+                >
+
+                </Box>
+                {isNonMobileScreens && (
+                    <Box
+                        flexBasis="26px"
+                    >
+
+                    </Box>
+                )}
+            </Box>
         </Box>
     );
 }
